cd3f648f08794e8bdf649abd88fa00e88790ac08
Brian Mosier <bmosier@gmail.com>
2020-06-24 18:12:26 -0400

    make playlist client method and hook into app

--- index.py
+++ index.py
@@ -1,15 +1,17 @@
 import datetime
 from flask import Flask, jsonify
+import vlc_client.vlc_client as VLC
 
 app = Flask(__name__)
+# TODO 6.24.2020: I Think this is where we set env vars.
+# write a config loader class to handle this safely.
+
 
 @app.route('/')
 def index():
-
     # Get whats playing
     # Get the current playlist
     # Adjust list
-
     return 'this is the sechedule'
 
 
@@ -17,17 +19,8 @@ def index():
 # stats as a json object
 @app.route('/current')
 def current():
-    # should return json with
-    #   title
-    #   total duration (seconds)
-    #   elapsed time (seconds)
-    # maybe this should be back channel to prevent Ddos?
-    resp = {
-        "title": "test title",
-        "duration": 3600,
-        "elapsed": 1800
-    }
-    return jsonify(resp)
+    return jsonify(
+        VLC.get_status('http://127.0.0.1:8080/requests/status.json'))
 
 
 @app.route('/playlist')
@@ -35,10 +28,7 @@ def playlist():
     # returns a json list of vidoes with
     #   title
     #   duration
-    #   expected air time
-    resp = [{
-        "title": "test title",
-        "duration": 3600,
-        "air_date": datetime.datetime.now()
-    }]
-    return jsonify(resp)
+    #   expected air time | missing right nowm maybe should be part
+    #     of the schedule
+    return jsonify(
+        VLC.get_playlist('http://127.0.0.1:8080/requests/playlist.json'))
--- vlc_client/vlc_client.py
+++ vlc_client/vlc_client.py
@@ -3,26 +3,28 @@ import http.client
 from urllib.parse import urljoin
 
 
-## TODO 6.24.2020: remove teh conf stuff from where
+# TODO 6.24.2020: remove teh conf stuff from where
+# and implement proper config management
+# should remove url params from client arguements.
+# maybe make a master config yml file or something
+
 # Config
 base_url = 'http://127.0.0.1'
 port = '8080'
 url = "{}:{}".format(base_url, port)
 status_endpoint = '/requests/status.json'
 playlist_endpoint = '/requests/playlist.json'
-
 status = urljoin(url, status_endpoint)
 
 
-
-
+# returns a status of currently playing frmo your status endpoint
+# {
+#   "title": <string>,
+#   "duration": <int>,
+#   "current": <int>"]
+# }
 def get_status(status_url):
-    json = {}
-    try:
-        response = make_request(status_url)
-        json = response.json()
-    except:
-        raise Exception("failed to get status: {}".format(status_url))
+    json = get_response_json(status_url)
     return {
       "title": json["information"]["category"]["meta"]["filename"],
       "duration": json["length"],
@@ -30,8 +32,34 @@ def get_status(status_url):
     }
 
 
-def make_request(url):
+# returns a playlist of videos
+# [{'duration': 8015, 'title': 'UCB_SEASON 1 disc 1.avi', 'id': '3'},
+#  {'duration': 5345, 'title': 'UCB_SEASON 1 disc 2.avi', 'id': '4'}]
+def get_playlist(playlist_url):
+    playlist = []
+    json = get_response_json(playlist_url)
+    for child in json["children"]:
+        if child["name"] == "Playlist":
+            for subchild in child["children"]:
+                playlist.append({
+                    "title": subchild["name"],
+                    "duration": subchild["duration"],
+                    "id": subchild['id']
+                })
+            break
+    return playlist
+
+
+def get_response_json(url):
     # TODO 6.24.2020: get this pw out of here
+    # TODO 6.24.2020: refactor to use `with requests.get` syntax
     auth = requests.auth.HTTPBasicAuth('', 'pass')
-    response = requests.get(url, auth=auth)
-    return response
+    json = {}
+    try:
+        with requests.Session() as s:
+            response = s.get(url, auth=auth)
+            print(response.status_code)
+            json = response.json()
+    except:
+        raise Exception("failed request: {}".format(url))
+    return json
